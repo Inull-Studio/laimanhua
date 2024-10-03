22c53737856c973f8758268271248f91fcba0d92
basket-ball <2293830442@qq.com>
2024-10-03 19:15:44 +0800

    fix picture crash

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from hashlib import md5
 from shutil import move
 from urllib.parse import urljoin, urlsplit
 from bs4 import BeautifulSoup
-import requests, sys, tempfile, time, os, json
+import requests, sys, tempfile, time, os, json, random
 
 
 class Laimanhua:
     location = os.path.join(os.path.dirname(sys.argv[0]), 'downloads')
     proxies = {}
     url = 'https://m.laimanhua8.com/'
-    picurl = 'https://mhpicwwt.kingwar.cn/'
+    picurls = [
+        'https://xwdf.kingwar.cn', 'https://mhreswhm.kingwar.cn',
+        'https://qwe123.kingwar.cn', 'https://resmhpic.kingwar.cn',
+        'https://reszxc.kingwar.cn']
+    picurl = random.choice(picurls)
     searchurl = 'https://m.laimanhua8.com/e/search/'
 
-    header = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'}
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'}
     picheader = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
-        'Referer': url}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
+        'Referer': 'https://m.laimanhua8.com/'}
     searchheader = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
         'Origin': url}
 
     def __init__(self, search_kw: str) -> None:
@@ -40,6 +44,10 @@ class Laimanhua:
         return result
 
     def parse_chapter(self, comic_url: str, comicname: str) -> list[dict]:
+        self.comiclocate = os.path.join(self.location, comicname)
+        if not os.path.exists(self.comiclocate):
+            os.mkdir(self.comiclocate)
+
         r = requests.get(comic_url, headers=self.header, proxies=self.proxies)
         r.encoding = 'gb2312'
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -51,11 +59,11 @@ class Laimanhua:
 
     def get_pic(self, chapter: dict[str, str]) -> list[str]:
         chaptername, url = chapter.copy().popitem()
-        self.chapterlocate = os.path.join(self.location, chaptername)
+        self.chapterlocate = os.path.join(self.comiclocate, chaptername)
         if not os.path.exists(self.chapterlocate):
             os.mkdir(self.chapterlocate)
 
-        r = requests.get(url, headers=self.header)
+        r = requests.get(url, headers=self.header, proxies=self.proxies)
         r.encoding = 'gb2312'
         soup = BeautifulSoup(r.text, 'html.parser')
         scripts = soup.find_all('script')
@@ -63,7 +71,7 @@ class Laimanhua:
         info = json.loads('{' + info + '}')
         picurls = []
         for u in info['images']:
-            picurl = urljoin(urljoin(self.picurl, info['path']), u)
+            picurl = urljoin(urljoin(self.picurl, info['path'].replace('・', '·')), u)
             picurls.append(picurl)
         return picurls
 
@@ -71,7 +79,7 @@ class Laimanhua:
         piclocate = os.path.join(self.chapterlocate, picurl.split('/')[-1].strip())
         if os.path.exists(piclocate):
             return
-        r = requests.get(picurl, headers=self.picheader)
+        r = requests.get(picurl, headers=self.picheader, proxies=self.proxies)
         tmpf = tempfile.NamedTemporaryFile('wb', delete=False)
         tmpf.write(r.content)
         tmpf.close()
@@ -85,14 +93,14 @@ class Laimanhua:
         index = int(input('请输入您要下载的漫画id:'))
         time.sleep(1)
 
-        comicurl, comicname = result[index - 1].popitem()
+        comicname, comicurl = result[index - 1].popitem()
         chapter_urls = self.parse_chapter(comicurl, comicname)
         time.sleep(1)
         for chapter in chapter_urls:
             results: list[Future] = []
             pics = self.get_pic(chapter)
             chaptername, _ = chapter.popitem()
-            time.sleep(0.5)
+            time.sleep(1)
             with ThreadPoolExecutor(4) as execute:
                 for pic in pics:
                     res = execute.submit(self.download, pic)
